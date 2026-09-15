e25b304ca30673e30e94e00a68ab073979b7b8eb
stifer <1577574695@qq.com>
2026-09-15 11:35:51 +0800

    chore: cleanup reproduce tests

--- test_reproduce.mjs
+++
@@ -1,4 +0,0 @@
-import { getDagDetail } from './packages/dsh-interactive-spec/src/history-service.ts';
-
-const res = getDagDetail('tutor_monitor_web_v1', 'D:\\myprogram\\InsightTutor');
-console.log('Result:', JSON.stringify(res, null, 2));
--- tests/reproduce.test.ts
+++
@@ -1,9 +0,0 @@
-import { handleDagRequest } from './packages/dsh-interactive-spec/src/routes';
-import { test } from 'vitest';
-
-test('reproduce handleDagRequest', async () => {
-  const req = new Request('http://127.0.0.1:3080/api/agentflow/dag?cwd=D:%5Cmyprogram%5CInsightTutor&dag_id=tutor_monitor_web_v1');
-  const res = await handleDagRequest(req);
-  const data = await res.json();
-  console.log('handleDagRequest data:', data);
-});
